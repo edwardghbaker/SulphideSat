76ac70c5e12b29857a07d9d0769e894d2461d6ac
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2022-12-28 22:23:28 +0000

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   SulphideSat.py

---
+++ SulphideSat.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from ChemCal_v2 import CC
+
+#%%
+
+class SCSS:
+
+    '''
+    input example for enstaite chondrite
+
+    EC_wt = pd.DataFrame(index = ['O','Si','Mg','Al','Ca','Fe','Ti','Mn','Cr','Na','K'],
+                            data = [48.0,29.7,19.5,1.36,0.17,0.33,0.01,0.03,0.13,1.04,0.09],
+                            columns = ['Wt. %'])
+    
+    '''
+
+    def __init__(self,CC,inputDF,Tc,Pgpa,log_fo2):
+        
+
+
+
+    def computeSCSS(self):
+
+    
