f78cbb05711758693daca16a46dc91ba84bb0c2e
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2022-12-30 21:08:11 +0000

    Trying to use docker container

--- SulphideSat.py
+++ SulphideSat.py
@@ -1,7 +1,9 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from ChemCal_v2 import CC
+from ThermoEngine.thermoengine.thermoengine import phases
 
 #%%
 
@@ -17,10 +19,32 @@ class SCSS:
     '''
 
     def __init__(self,CC,inputDF,Tc,Pgpa,log_fo2):
+
+    
+
+    def computeActivities(self):
+
         
 
+        return Am
 
 
     def computeSCSS(self):
+        '''
+        This equation is taken directly from the Smythe paper.
+        '''
 
-    
+        Tk = self.Tc + 273.15
+        B_prime = self.B_prime
+        C = self.C
+        P = self.Pgpa*1e9
+        Xm = self.Xm
+        Am = self.Am
+        a_FeS = self.a_FeS
+        a_FeO = self.a_FeO
+        
+        S_ppm  = 14695/Tk - 9.656 + 1.02*np.log(Tk) + B_prime + ((C*P)/Tk)*np.sum((Xm*Am)/Tk) + np.log(a_FeS) - np.log(a_FeO)
+        return S_ppm 
+
+
+# %%
