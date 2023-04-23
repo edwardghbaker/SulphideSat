b01826c515410f8d3a89ca22bad08a17048bcdf9
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2023-04-23 16:47:56 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   SulphideSat.py
    
    Changes not staged for commit:
    	modified:   __pycache__/ChemCal_v2.cpython-311.pyc

--- SulphideSat.py
+++ SulphideSat.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from ChemCal_v2 import CC
-from ThermoEngine.thermoengine.thermoengine import phases
+#from ChemCal_v2 import CC
+#from ThermoEngine.thermoengine.thermoengine import phases
 
 #%%
 
@@ -18,11 +18,29 @@ class SCSS:
     
     '''
 
-    def __init__(self,CC,inputDF,Tc,Pgpa,log_fo2):
+    def __init__(self,comp,Tc,Pgpa,log_fo2):
+            
+            self.comp = comp
+            self.Tc = Tc
+            self.Pgpa = Pgpa
+            self.log_fo2 = log_fo2
+    
+            self.Xm = self.computeXm()
+            self.Am = self.computeActivities()
+            self.a_FeS = self.computeFeS()
+            self.a_FeO = self.computeFeO()
+            self.B_prime = self.computeBprime()
+            self.C = self.computeC()
+            self.S_ppm_sat = self.computeSCSS()
 
     
 
     def computeActivities(self):
+        '''
+        '''
+
+
+
 
         
 
@@ -43,8 +61,33 @@ class SCSS:
         a_FeS = self.a_FeS
         a_FeO = self.a_FeO
         
-        S_ppm  = 14695/Tk - 9.656 + 1.02*np.log(Tk) + B_prime + ((C*P)/Tk)*np.sum((Xm*Am)/Tk) + np.log(a_FeS) - np.log(a_FeO)
+        S_ppm  = 14695/Tk - 9.656 + 1.02*np.log(Tk) + B_prime + ((C*P)/Tk)*np.sum(np.dot(Xm*Am)/Tk) + np.log(a_FeS) - np.log(a_FeO)
         return S_ppm 
 
 
 # %%
+
+def SCSS_oneill(Xca=0,Xmg=0,Xna=0,Xk=0,Xti=0,Xfe=0,Xal=0):
+     '''
+     This function computes the sulphide capacity (ppm) for a given composition. As reported by O'neill, Hugh S. T. C., and John A. Mavrogenes. 2002. “The Sulfide Capacity and the Sulfur Content at Sulfide Saturation of Silicate Melts at 1400°C and 1 Bar.” Journal of Petrology 43 (6): 1049-87.
+     
+
+     this relationship is derived from 1atm experiments done at 1400C. The composition is in wt. %.
+     '''
+
+     
+     A0 = -5.018
+     Aca = 7.56
+     Amg = 4.48
+     Anak = 4.24
+     Ati = 5.20
+     Afe = 26.31
+     Aal = 1.06
+     Bfeti = 48.48
+
+     return np.exp(A0+Xca*Aca+Xmg*Amg+(Xna+Xk)*Anak+Xti*Ati+Xfe*Afe+Xal*Aal+Xfe*Xti*Bfeti)
+     
+
+     
+
+
